88780d7af0de5dffb7c1f444327ab7cd9ae3528e
mr.Omar Faruk <Hazrat.omurfaruk@gmail.com>
2023-03-16 19:50:36 +0600

    create new branch

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,36 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         primarySwatch: Colors.green,
       ),
-      home: const Text('hello world'),
+      home: const GetData(),
+    );
+  }
+}
+
+class GetData extends StatelessWidget {
+  const GetData({super.key});
+  @override
+  Widget build(BuildContext context) {
+    var firebaseDb = FirebaseFirestore.instance.collection('diaryBook');
+
+    return Scaffold(
+      body: StreamBuilder(
+          stream: firebaseDb.snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text('something was wrong');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text('loading...');
+            }
+            print(snapshot.data!.docs[0]['title']);
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                      title: Text(snapshot.data!.docs[index]['author']),
+                      subtitle: Text(snapshot.data!.docs[index]['title']));
+                });
+          }),
     );
   }
 }
