f1bed1d8ff0271198a6b7fb74e37dda8e1394a28
mr.Omar Faruk <Hazrat.omurfaruk@gmail.com>
2023-03-16 19:48:32 +0600

    add firebase in project

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@ import 'package:diary_book/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
