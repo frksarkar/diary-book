8deb3851e339d270ad8597aa37887d90bba7a2b3
faruksarkar <hazrat.omurfaruk@gmail.com>
2023-04-04 06:08:45 +0600

    create timestamp function

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,1 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
+String formatDate(Timestamp date) {
+  return DateFormat.yMMMMd().format(date.toDate()).toString();
+}
+
+String formatDateTimestamp(Timestamp? timestamp) {
+  return DateFormat.yMMMd().add_EEEE().format(timestamp!.toDate());
+}
+
+String formatDateFromTimestampHour(Timestamp? timestamp) {
+  return DateFormat.jm().format(timestamp!.toDate());
+}
